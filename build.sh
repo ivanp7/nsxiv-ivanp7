72a741d0fad47270a0d04d9e817885d1c5af44ec
ivanp7 <ivanp7@mail.ru>
2022-11-03 15:37:50 +0300

    initial commit

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+cd -- "$(dirname -- "$0")"
+
+DIR="$PWD"
+
+git clone "https://aur.archlinux.org/nsxiv.git" nsxiv && cd nsxiv || exit 1
+sed -i 's/^pkgname=.*$/pkgname=nsxiv-ivanp7/' PKGBUILD
+echo "provides=('nsxiv')" >> PKGBUILD
+makepkg --noconfirm -o
+
+cd src/nsxiv/
+
+install -m 644 -t . "$DIR/config.h" "$DIR/nsxiv.1" "$DIR/keycodes.patch"
+patch < keycodes.patch
+
+cd ../..
+
+makepkg --noconfirm -efsr && mv *.pkg.tar.zst ../
+
